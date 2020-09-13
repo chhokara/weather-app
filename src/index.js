2fb335c459e46bdb347d116b3d3d25de883d1b2c
Lucas <lucaswong.ca@gmail.com>
2020-09-12 18:48:05 -0700

    Set up component folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello world!");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

8689cc60f15be83821de4b2738e10d9fd4b9432e
Cam Way <cway140@gmail.com>
2020-09-17 08:57:56 -0700

    added autocomplete suggestions using google API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,45 @@
 import React from "react";
 import {getPosition} from "./../geolocation"
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng,
+} from 'react-places-autocomplete'; 
 
 export default function SearchBar() {
   getPosition();
+  const [address, setAddress] = React.useState("");
+  const handleSelect = async value => {};
   return (
     <div className="m-12 flex">
       <svg className="h-6 w-6 m-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       </svg>
-      <input className="bg-transparent outline-none placeholder-primary placeholder-opacity-100 overflow-hidden" placeholder=
-      "Search for places...">
-      </input>
+
+      {/* source: https://www.youtube.com/watch?v=uJYqQdnw8LE */}
+      <PlacesAutocomplete
+      value={address}
+      onChange={setAddress}
+      onSelect={handleSelect}>{({ getInputProps, suggestions, getSuggestionItemProps, loading})=>(
+        <div>
+          <input className="h-10 outline-none placeholder-primary placeholder-opacity-100 overflow-hidden" {...getInputProps({ placeholder: "Search for places..."})}/>
+            <div>
+              {loading ? <div>...loading</div>: null}
+              
+              {suggestions.slice(0,4).map((suggestion, index) => {
+                return <div  key={index} {...getSuggestionItemProps(suggestion)}>{suggestion.description}</div>
+              })}
+            </div>
+        </div>
+      )}</PlacesAutocomplete>
 
       {/* Display Location Button*/}
       <button className="h-8 w-8 p-1 bg-gray-200 rounded-full" onClick={() => {
-          var latitude = localStorage.getItem('latitude65');
-          var longitude = localStorage.getItem('longitude65');
+          let latitude = localStorage.getItem('latitude');
+          let longitude = localStorage.getItem('longitude');
           alert("Position: " + latitude + ", " + longitude);
         }}>
-          
+
         <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
--- src/geolocation.js
+++ src/geolocation.js
@@ -1,15 +1,15 @@
 export const getPosition = () => {
     if (navigator.geolocation){  
-      navigator.geolocation.getCurrentPosition(showGPS);
+      navigator.geolocation.getCurrentPosition(storeCoordinates);
     } else {  
         //no location found
         alert("Geolocation no Supported")
     }
 }
 
-function showGPS(position){
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    localStorage.setItem('latitude65', JSON.stringify(latitude));
-    localStorage.setItem('longitude65', JSON.stringify(longitude));
+function storeCoordinates(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    localStorage.setItem('latitude', JSON.stringify(latitude));
+    localStorage.setItem('longitude', JSON.stringify(longitude));
 }

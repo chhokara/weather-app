438b4a75aa50f0bc1e652c4cb367bf0960416ffd
Lucas <lucaswong.ca@gmail.com>
2020-09-17 22:32:23 -0700

    Add box styling for each highlight

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -9,14 +9,18 @@ import AirQual from "./AirQual";
 
 export default function Highlights() {
   return (
-    <div className="m-5 bg-red-300">
-      <h2>Today's Highlights</h2>
-      <UVIndex />
-      <WindStatus />
-      <SunRiseSet />
-      <Humidity />
-      <Visibility />
-      <AirQual />
+    <div className="m-10 w-7/12 h-full p-5 bg-gray-300">
+      <h2 className="text-lg text-purple-black font-extrabold">
+        Today's Highlights
+      </h2>
+      <div className="flex flex-wrap">
+        <UVIndex />
+        <WindStatus />
+        <SunRiseSet />
+        <Humidity />
+        <Visibility />
+        <AirQual />
+      </div>
     </div>
   );
 }
--- src/components/Highlights/Humidity.js
+++ src/components/Highlights/Humidity.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Humidity() {
-  return <div>Hello</div>;
+  return (
+    <div
+      className="m-5 h-48 w-64 
+      flex justify-center 
+      align-center rounded-lg bg-white"
+    >
+      Hello
+    </div>
+  );
 }
